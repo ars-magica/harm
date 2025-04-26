fa64cf5fd31c4c3f5460542300e267a5c7566a6e
Hans Georg Schaathun <georg@schaathun.net>
2025-04-26 17:14:17 +0200

    Updates to Lab

--- src/ArM/Types/Lab.hs
+++ src/ArM/Types/Lab.hs
@@ -20,6 +20,7 @@ import Data.Maybe
 import Data.List
 
 import ArM.Helper
+import ArM.Types.HarmKey
 
 import ArM.Debug.Trace
 
@@ -40,16 +41,8 @@ instance FromJSON Lab
         <*> v .:? "history" .!= []
         <*> v .:? "plan" .!= []
 
--- | ID of a Lab, to reference it independently of the state.
-data LabID = LabID String
-    deriving ( Show, Ord, Eq, Generic )
-
-instance ToJSON LabID
-instance FromJSON LabID
-
--- | get the ID of a character.
-labID :: Lab -> LabID
-labID = LabID . labName
+instance KeyObject Lab where
+    harmKey = CovenantKey . labName
 
 instance HarmObject Lab where
     name = labName 
@@ -135,98 +128,3 @@ defaultAdv = LabAdvancement
      , newSize = Nothung
      , labRefine = 0
      }
-instance ToJSON CovAdvancement
-instance FromJSON CovAdvancement where
-    parseJSON = withObject "CovAdvancement" $ \v -> CovAdvancement
-        <$> fmap parseSeasonTime ( v .:? "season" )
-        <*> v .:? "narrative" .!= []
-        <*> v .:? "joining" .!= []
-        <*> v .:? "leaving" .!= []
-        <*> v .:? "acquired" .!= []
-        <*> v .:? "lost" .!= []
-
-
--- |
--- The `Advance` instance is very similar to that of `Character`, but has to
--- be implemented separately to account for different advancement classes.
-instance Advance Covenant where
-   advance ct c | isNothing (covenantState c) = trace "need prepare" $ advance ct $ prepare c
-                | ct < ct' =  c
-                | otherwise =  advance ct $ step c 
-            where ct' =  nextSeason c
-
-   stepIf ns = trace ("stepIf (Cov): "++show ns) $ completeCovAdv . applyCovAdv . nextCovAdv ns
-
-   step cov = completeCovAdv $ applyCovAdv (new,Just y')
-            where (y:ys) = futureCovAdvancement cov
-                  y' = prepareAdvancement y
-                  new = cov { futureCovAdvancement = ys }
-   nextSeason = f . futureCovAdvancement
-       where f [] = NoTime
-             f (x:_) = caSeason x
-
--- | CovenFolk joining according to the augmented covenant advancement.
-joiningAug :: AugCovAdvancement -> [CharacterID]
-joiningAug (AugCovAdvancement a b) = a' ++ b'
-    where a' = fromMaybe [] $ fmap joining a
-          b' = fromMaybe [] $ fmap joining b
-
--- | CovenFolk leaving according to the augmented covenant advancement.
-leavingAug :: AugCovAdvancement -> [CharacterID]
-leavingAug (AugCovAdvancement a b) = a' ++ b'
-    where a' = fromMaybe [] $ fmap leaving a
-          b' = fromMaybe [] $ fmap leaving b
-
--- | Get the season of the augmented covenant advancement.
--- This is taken from the explicit advancement if available, and
--- the inferred advancement otherwise.
-caSeasonAug :: AugCovAdvancement -> SeasonTime
-caSeasonAug (AugCovAdvancement a b) = fromMaybe b' $ fmap caSeason a
-    where b' = fromMaybe NoTime $ fmap caSeason b
-
--- | Apply covenant advancement
-applyCovAdv :: (Covenant,Maybe AugCovAdvancement)
-         -> (Covenant,Maybe AugCovAdvancement)
-applyCovAdv (c,Nothing) = (c,Nothing)
-applyCovAdv (c,Just a) = (c',Just a)
-    -- where (a',st') = applyCovAdvancement a st
-    where st' = st { covTime = caSeasonAug a, covenFolkID = cid }
-          c' = c { covenantState = Just st' }
-          st = fromMaybe defaultCovState $ covenantState c
-          cid1 = sort $ joiningAug a ++ covenFolkID st 
-          cid = cid1 -= ( sort $ leavingAug a )
-
-
--- | Complete the advancement procedure to return the new Covenant with
--- the updated state.
-completeCovAdv :: (Covenant,Maybe AugCovAdvancement)
-                 -> Covenant
-completeCovAdv (c,Nothing) = c
-completeCovAdv (c,Just a) = c { pastCovAdvancement = a:pastCovAdvancement c }
-
--- |
--- Get the next augmented advancement.
-nextCovAdv :: SeasonTime -> Covenant -> (Covenant,Maybe AugCovAdvancement)
-nextCovAdv ns cov | fs == [] = (cov,Nothing)
-              | caSeason adv > ns = trace (show (ns,caSeason adv,covenantID cov)) $ (cov,Nothing)
-              | otherwise = (new,Just a)
-        where a = prepareAdvancement adv
-              (adv:as) = fs
-              fs = futureCovAdvancement cov
-              new = cov { futureCovAdvancement = as }
-
-
-prepareAdvancement :: CovAdvancement -> AugCovAdvancement
-prepareAdvancement a = AugCovAdvancement (Just a) Nothing
-
-contractAdvancement :: AugCovAdvancement -> CovAdvancement
-contractAdvancement aug  = CovAdvancement
-     { caSeason = season aug
-     , caNarrative = listFromMaybe caNarrative aa ++ listFromMaybe caNarrative ad
-     , joining = listFromMaybe joining aa ++ listFromMaybe joining ad
-     , leaving = listFromMaybe leaving aa ++ listFromMaybe leaving ad
-     , acquired = listFromMaybe acquired aa ++ listFromMaybe acquired ad
-     , lost = listFromMaybe lost aa ++ listFromMaybe lost ad
-     } 
-     where (AugCovAdvancement aa ad) = aug
-
