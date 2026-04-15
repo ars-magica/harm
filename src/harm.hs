144da6e0bb828c391ac9bbde5fce08302d244f3e
Hans Georg Schaathun <georg@schaathun.net>
2026-04-15 19:40:22 +0200

    description for doc's

--- src/harm.hs
+++ src/harm.hs
@@ -5,6 +5,17 @@
 -- License     :  see LICENSE
 --
 -- Maintainer  :  hg+gamer@schaathun.net
+-- 
+-- Description :  
+--   harm is a command-line interface to character generation for Ars Magica.
+--
+--   Characters and their advancement are specified in a JSON format, which
+--   is validated and converted to markdown file, showing the state of the
+--   characters at different points in time.
+--
+--   Covenant generation and advancement is also covered.
+--
+--   This is work in progress, and may features are missing.
 --
 -----------------------------------------------------------------------------
 
