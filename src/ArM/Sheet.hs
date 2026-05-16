c73df3b59ae8e6a7e386ca886fe069ddcf57639a
Hans Georg Schaathun <georg@schaathun.net>
2026-05-16 21:41:35 +0200

    added missing file

---
+++ src/ArM/Sheet.hs
@@ -0,0 +1,17 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  ArM.Sheet
+-- Copyright   :  (c) Hans Georg Schaathun <hg+gamer@schaathun.net>
+-- License     :  see LICENSE
+--
+-- Maintainer  :  hg+gamer@schaathun.net
+--
+-- Description :  Functions to extract data for sheets.
+--
+-- This does not produce output, but organises the information in
+-- an abstract format prior to printing.
+--
+-----------------------------------------------------------------------------
+module ArM.Sheet ( Library(..), getLibrary ) where
+
+import ArM.Sheet.Library
